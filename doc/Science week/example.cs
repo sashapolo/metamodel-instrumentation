f7ea37d2778a4381a720e8a171342f8f53d1e548
Snowball <sashapolo239@gmail.com>
2013-11-26 20:01:58 +0400

    some documents

---
+++ doc/Science week/example.cs
@@ -0,0 +1,6 @@
+public class Car : Vehicle {
+    private Vendor vendor;
+    public Customer customer { get; set; }
+
+    private double calculateMaxSpeed(int foo) {}
+}
---
+++ doc/Science week/example.java
@@ -0,0 +1,9 @@
+public class Car implements Vehicle {
+    private Vendor vendor;
+    private Customer customer;
+
+    public Customer getCustomer() {}
+    public void setCustomer(Customer customer) {}
+
+    public double calculateMaxSpeed(int foo) {}
+}
