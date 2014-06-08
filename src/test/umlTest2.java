8b09679702e32f9ab9e47def4966d96a82865cc9
Snowball <sashapolo239@gmail.com>
2014-06-09 00:53:20 +0400

    presentation

---
+++ src/test/umlTest2.java
@@ -0,0 +1,19 @@
+public class umlTest {
+    private final Test2 reference;
+    private final Test3[] multipleReference;
+
+    public void public1() {}
+
+    public static void main(final String[] args) {}
+}
+
+class Test2 extends Super {
+    private final int foo;
+}
+
+class Test3 extends Super {
+    private final String bar;
+}
+
+class Super {
+}
